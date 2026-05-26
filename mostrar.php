aa443b7da46fb6ded65628f635c7a7b1732cccc1
MigueLunaa007 <252310055@itslerdo.edu.mx>
2026-05-26 16:12:29 -0600

    El desafio resuelto nos permite modificar el programa para usar una lista ordenada en lugar de contar manualmente con la variable

--- mostrar.php
+++ mostrar.php
@@ -8,7 +8,7 @@ $archivo = "asistentes.txt";
 try{
     // verifica si el archivo existe
     if (!file_exists($archivo)){
-        throw new Exception("El archivo no existe.");
+        throw new Exception("El archivo no existe. Por favor, registra asistentes primero.");
     }
 
     // abrir el archivo para lectura
@@ -19,25 +19,38 @@ try{
         throw new Exception("No se pudo abrir el archivo para lectura.");
     }
 
+    echo "<h2>Lista Oficial de Asistentes</h2>";
+    // aqui se inicia la etiqueta de lista ordenada HTML
+    echo "<ol>";
+
     // leer el archivo linea por linea y mostrar los nombres
     $contador = 1;
     while (!feof($fp)){
         // leemos una linea del archivo
         $linea = fgets($fp);
         
-        // verificamos que la linea no este vacia antes de imprimir
+        // evitamos imprimir "<li></li>" vacios al final del archivo
+        if (trim($linea) !== '') {
+            // imprimimos cada nombre dentro de una etiqueta de elemento de lista <li>
+            echo "<li>" . htmlspecialchars(trim($linea)) . "</li>";
+        }
+
+        /* // verificamos que la linea no este vacia antes de imprimir
         if (trim($linea) !==''){
             // htmlspecialchars() para evitar problemas de seguridad con HTML
              echo $contador . ". " . htmlspecialchars(trim($linea)) . "<br>";
             $contador++; 
-        }
+        }*/
 
     }
+
+    // cerramos la etiqueta de lista ordenada
+    echo "</ol>";
 
     fclose($fp);
 }
 
 catch (Exception $e){
-    echo "Error: " . $e->getMessage();
+    echo "<strong>Error:</strong> " . $e->getMessage();
 }
 ?>
